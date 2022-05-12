c3014ad84e6a7420e3f3362b5d9b22910e08200e
LuiguiHer <hernandez4389562@gmail.com>
2022-05-12 13:07:30 -0500

    activity Welcome

---
+++ app/src/main/java/com/example/ome/Welcome.kt
@@ -0,0 +1,19 @@
+package com.example.ome
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val button3 = findViewById<Button>(R.id.btn_newLogin)
+        button3.setOnClickListener {
+            val start = Intent(this, MainActivity::class.java)
+            startActivity(start)
+        }
+    }
+}
