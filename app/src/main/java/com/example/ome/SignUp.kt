0c198dacd7e74e9670dbf28a2f4cb86c06907557
LuiguiHer <hernandez4389562@gmail.com>
2022-05-16 16:46:39 -0500

    sharedReference of Sing_Up

--- app/src/main/java/com/example/ome/SignUp.kt
+++ app/src/main/java/com/example/ome/SignUp.kt
@@ -1,20 +1,55 @@
 package com.example.ome
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
+import androidx.appcompat.widget.SwitchCompat
+import com.google.android.material.textfield.TextInputLayout
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var sharedPreferences: SharedPreferences
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
 
+        sharedPreferences = getSharedPreferences("REGISTER_DATA", Context.MODE_PRIVATE)
+
+
         val register = findViewById<Button>(R.id.btn_register)
         register.setOnClickListener {
+
+            val name = findViewById<EditText>(R.id.text_name).text.toString()
+            val lastName = findViewById<EditText>(R.id.text_lastName).text.toString()
+            val email = findViewById<AutoCompleteTextView>(R.id.autoEmail_address).text.toString()
+            val year = findViewById<Spinner>(R.id.spinner_years).selectedItem.toString()
+            val month = findViewById<Spinner>(R.id.spinner_month).selectedItem.toString()
+            val day = findViewById<EditText>(R.id.number_day).toString()
+            val street = findViewById<TextInputLayout>(R.id.textInputLayout).toString()
+            val city = findViewById<TextInputLayout>(R.id.textInputLayout2).toString()
+            val state = findViewById<Spinner>(R.id.spn_state).selectedItem.toString()
+            val terms = findViewById<SwitchCompat>(R.id.agreeT).isChecked
+
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putString("NAME", name)
+            editor.putString("LAST_NAME", lastName)
+            editor.putString("EMAIL", email)
+            editor.putString("YEAR", year)
+            editor.putString("MONTH", month)
+            editor.putString("DAY", day)
+            editor.putString("STREET", street)
+            editor.putString("CITY", city)
+            editor.putString("STATE", state)
+            editor.putBoolean("TERMS", terms)
+            editor.apply()
+
+            Toast.makeText(this, "Successful Registration", Toast.LENGTH_SHORT).show()
             val start = Intent(this, Welcome::class.java)
             startActivity(start)
         }
@@ -33,7 +68,6 @@ class SignUp : AppCompatActivity() {
             listY
         )
         spinnerY.adapter = adapterY
-
 
         val spinnerM = findViewById<Spinner>(R.id.spinner_month)
         val listM = resources.getStringArray(R.array.month)
--- app/src/main/java/com/example/ome/Welcome.kt
+++ app/src/main/java/com/example/ome/Welcome.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import android.widget.Button
 
 class Welcome : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
 
         val button3 = findViewById<Button>(R.id.btn_newLogin)
         button3.setOnClickListener {
